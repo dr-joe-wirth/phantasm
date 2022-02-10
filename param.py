7b78058a56ecd6f944b7ff9f2ba5dda86675e87d
dr-joe-wirth <jwirth@hmc.edu>
2022-02-10 10:16:24 -0800

    initial commit

---
+++ param.py
@@ -0,0 +1,16 @@
+# specify the number of processors to use
+NUM_PROCESSORS:int = 3
+
+# specify the maximum number of taxa in a given analysis
+MAX_LEAVES:int = 50
+
+# modifying these files may cause phantasm to fail
+BLASTPLUS_DIR:str = '/usr/local/ncbi/blast/bin'
+MUSCLE_EXE:str = '/usr/local/bin/muscle'
+FASTTREE_EXE:str = '/usr/local/bin/FastTree'
+PHANTASM_DIR:str = '/Users/jwirth/Documents/phantasm'
+XENOGI_DIR:str = '/Users/jwirth/Documents/xenoGI'
+CSV_1:str = '/Users/jwirth/Documents/phantasm/resources/lpsn_gss_2022-02-01.csv'
+CSV_2:str = '/Users/jwirth/Documents/phantasm/resources/genusParents.csv'
+CSV_3:str = '/Users/jwirth/Documents/phantasm/resources/validPhylumClassOrderFamily.csv'
+CSV_4:str = '/Users/jwirth/Documents/phantasm/resources/synonymsPhylumClassOrderFamily.csv'
