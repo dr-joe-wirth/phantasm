25f8633b26f5678a1145edebb7fea1df14e51afc
dr-joe-wirth <jwirth@hmc.edu>
2022-10-12 15:17:48 -0700

    major changes to resolve a bug with test case 24:   * added bew function __resolveCollidingRoots   * major addition to constructTaxonomy to resolve colliding roots

--- PHANTASM/taxonomy/taxonomyConstruction.py
+++ PHANTASM/taxonomy/taxonomyConstruction.py
@@ -33,32 +33,75 @@ def constructTaxonomy(taxids:list, saveTax:bool=False, dir:str='./') -> Taxonomy
     # retrieve a list of Taxonomy objects at the order-level 
     # that are represented by the input taxids
     print(PRNT_1)
-    allOrders = __initializeTaxonomy(taxids)
+    allOrdersL = __initializeTaxonomy(taxids)
 
     # raise an error if no orders were found
-    if len(allOrders) == 0:
+    if len(allOrdersL) == 0:
         raise RuntimeError(ERR_MSG_1)
 
-    # for each order ...
     print(PRNT_2, end='', flush=True)
     lpsnD = _getLpsnData()
-    for idx in range(len(allOrders)):
-        # ... reconcile the object with LPSN data
-        order:Taxonomy = allOrders[idx]
+
+    # initialize a dictionary to resolve colliding root taxids
+    rootTaxD = dict()
+
+    # for each order ...
+    for idx in range(len(allOrdersL)):
+        # reconcile the object with LPSN data
+        #### note: this step can produce roots that will collide
+        order:Taxonomy = allOrdersL[idx]
         order._addLpsnData(lpsnD)
 
-        # ... make sure the object is at its root
-        allOrders[idx] = order.getRoot()
+        # create a new entry for the taxid if it is not in the dictionary
+        if order.getRoot().taxid not in rootTaxD.keys():
+            rootTaxD[order.getRoot().taxid] = []
+
+        # store the current index under the taxid 
+        rootTaxD[order.getRoot().taxid].append(idx)
+
+        # make sure all objects are at their roots
+        allOrdersL[idx] = order.getRoot()
+
+    # initialize a list of indices that will need to be removed from allOrdersL
+    indicesToRemoveL = list()
+    
+    # check the taxids in in the dictionary
+    for taxid in rootTaxD.keys():
+        # get the last index seen for this taxid
+        idx = rootTaxD[taxid].pop()
+
+        # use the index to get the fist seen taxonomy object
+        taxO:Taxonomy = allOrdersL[idx]
+
+        # resolve any roots whose taxids are colliding
+        # this loop will only be entered if collisions will occur
+        for idx in rootTaxD[taxid]:
+            # get the offending  taxonomy
+            nextO:Taxonomy = allOrdersL[idx]
+
+            # resolve the collisions
+            __resolveCollidingRoots(taxO, nextO)
+
+            # mark the index as flagged for removal
+            indicesToRemoveL.append(idx)
+
+    # sort the indices from high-to-low for on-the-fly popping
+    indicesToRemoveL = sorted(indicesToRemoveL, reverse=True)
+
+    # remove the specificied objects from allOrdersL
+    for idx in indicesToRemoveL:
+        allOrdersL.pop(idx) 
+
     print(DONE)
     
     # if only one order was made, then use it
-    if len(allOrders) == 1:
-        taxonomy = allOrders.pop()
+    if len(allOrdersL) == 1:
+        taxonomy = allOrdersL.pop()
     
     # if multiple orders were made, then merge them into a single object
-    elif len(allOrders) > 1:
+    elif len(allOrdersL) > 1:
         print(PRNT_3, end='', flush=True)
-        taxonomy = Taxonomy._mergeMultipleRoots(allOrders, lpsnD)
+        taxonomy = Taxonomy._mergeMultipleRoots(allOrdersL, lpsnD)
         print(DONE)
     
     # otherwise, no orders were made; raise an error
@@ -157,6 +200,64 @@ def _getLpsnData() -> dict:
 
     # import LPSN data
     return makeLpsnD(CSV_1, CSV_2, CSV_3, CSV_4)
+
+
+def __resolveCollidingRoots(taxO:Taxonomy, otherO:Taxonomy) -> None:
+    """ resolveCollidingRoots:
+            Accepts two taxonomy objects whose taxids are equal. Resolves any
+            collisions by coercing the second object's data into the first obj-
+            ect. Accomplishes this by recursing along shared children of the 
+            two objects. Modifies both inputs. Does not return.
+    """
+    # constants
+    ERR_MSG_1 = "objects do not share taxids"
+    ERR_MSG_2 = "untested condition: artificial taxids are colliding"
+    ERR_MSG_3 = "untested condition: taxid present, but name doesn't exist"
+    ERR_MSG_4 = "untested condition: synonym names don't match"
+
+    # ensure that the taxids of the two objects are identical
+    if taxO.taxid != otherO.taxid:
+        raise RuntimeError(ERR_MSG_1)
+
+    # only do work if objects are inequivalent
+    if taxO != otherO:
+        # for each child of the second object
+        otherChild:Taxonomy
+        for otherChild in otherO.getChildren(list):
+            # if the taxid is not already present in taxO
+            if otherChild.taxid not in taxO:
+                # if the scientific name is not already present in taxO
+                if otherChild.sciName not in taxO:
+                    # simply import the descendant; no collisions will occur
+                    taxO._importDirectDescendant(otherChild)
+                
+                # if the scientific name is already present in taxO
+                else:
+                    # get the synonymous child from taxO
+                    taxChild = taxO.getDescendantBySciName(otherChild.sciName)
+
+                    # artificial ids colliding here is untested. raise an error
+                    if int(taxChild.taxid) < 0 or int(otherChild.taxid) < 0:
+                        raise RuntimeError(ERR_MSG_2)
+
+                    # import the descendant, then resolve the duplicate names
+                    taxO._importDirectDescendant(otherChild)
+
+            # taxid present with the name absent is an untested condition
+            elif otherChild.sciName not in taxO:
+                raise RuntimeError(ERR_MSG_3)
+
+            # if taxid is present in taxO 
+            else:
+                # get the synonymous child from taxO
+                taxChild = taxO.getDescendantByTaxId(otherChild.taxid)
+
+                # make sure the names match; error if not
+                if taxChild.sciName != otherChild.sciName:
+                    raise RuntimeError(ERR_MSG_4)
+                
+                # recurse on the two synonymous children
+                __resolveCollidingRoots(taxChild, otherChild)
 
 
 def __saveTaxonomy(taxO:Taxonomy, dir:str) -> None:
