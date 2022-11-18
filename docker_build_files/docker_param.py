93e1467cdb061d11175c4c1938e9fb1b24ec2dc5
dr-joe-wirth <jwirth@hmc.edu>
2022-11-18 09:48:12 -0800

    updated Dockerfile and docker_param.py   * now using my fork of xenoGI (v3.1.1)   * Dockerfile       - build instructions now use fork       - help instructions modified to print `phantasm`   * docker_param.py       - xenoGI folder updated to remain coherent with new fork

--- docker_build_files/docker_param.py
+++ docker_build_files/docker_param.py
@@ -26,7 +26,7 @@ MUSCLE_EXE:str = '/exec/muscle'
 FASTTREE_EXE:str = '/exec/FastTreeMP'
 IQTREE_EXE:str = '/exec/iqtree/bin/iqtree'
 PHANTASM_DIR:str = '/phantasm'
-XENOGI_DIR:str = '/xenoGI-3.1.0'
+XENOGI_DIR:str = '/xenoGI-v3.1.1'
 
 # modifying these variables may cause phantasm to fail
 import os
