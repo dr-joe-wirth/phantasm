b2c84a3509fcec22756cc31c49f75ee075bd191e
dr-joe-wirth <dr.joe.wirth@gmail.com>
2023-11-07 11:43:51 -0500

    updated version in anticipation of new release

--- phantasm.py
+++ phantasm.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __author__ = "Joseph S. Wirth"
 
 from PHANTASM.main import getPhyloMarker, refinePhylogeny, knownPhyloMarker, analyzeSpecifiedGenomes, rankPhyloMarkers
